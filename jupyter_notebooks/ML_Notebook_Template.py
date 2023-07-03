c3fb7255ed2cb51733a21f5531c580a2d5898cbd
Oskar Johansson <92440552+OskarJ1993@users.noreply.github.com>
2023-07-03 14:31:22 +0200

    add for split the data for start train and test model

--- jupyter_notebooks/ML_Notebook_Template.py
+++ jupyter_notebooks/ML_Notebook_Template.py
@@ -24,3 +24,23 @@ data[features].head()
 # Correlation Study Summary
 correlation_matrix = data[features].corr()
 print(correlation_matrix)
+
+# Correlation Study
+plt.figure(figsize=(10, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm")
+plt.title("Correlation Study")
+plt.show()
+
+# Individual plots per variable
+for variable in features:
+    plt.figure(figsize=(6, 4))
+    sns.scatterplot(data=data, x=variable, y="SalePrice")
+    plt.title(f"{variable} vs. SalePrice")
+    plt.show()
+
+# Split the data into train and test sets
+X = data[features]
+y = data["SalePrice"]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=42
+)
